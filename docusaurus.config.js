26915500d66c989ef5c3aa93207596c66c6af4ac
rizkhal <lamaaurizkhal@gmail.com>
2022-08-07 03:00:51 +0800

    fix: base url

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -9,15 +9,15 @@ const config = {
   title: 'My Site',
   tagline: 'Dinosaurs are cool',
   url: 'https://lamaau.space',
-  baseUrl: '/',
+  baseUrl: '/lamaau',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'codegoen', // Usually your GitHub org/user name.
-  projectName: 'module-docs', // Usually your repo name.
+  organizationName: 'lamaau', // Usually your GitHub org/user name.
+  projectName: 'lamaau', // Usually your repo name.
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
