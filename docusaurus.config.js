74b9d0a81dc7faefd2801635f501b4441b8eadf1
rizkhal <lamaaurizkhal@gmail.com>
2022-08-07 03:21:26 +0800

    fix: base url

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,7 +8,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 const config = {
   title: 'My Site',
   tagline: 'Dinosaurs are cool',
-  url: 'https://lamaau.github.io',
+  url: 'https://www.lamaau.space',
   baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
